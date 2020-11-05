c00d02cbd9c23aaceefe253be147e5e9daf33bd2
Clayton Siby <claytonsiby@gmail.com>
2020-11-05 21:31:06 +0200

    Configure bootstrap & jquery plugins & add github linters

--- db/migrate/20201105125306_create_users.rb
+++ db/migrate/20201105125306_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :lastname
-      t.string :email
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_125306) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "lastname"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
