f41b7f0795e991901da65d5f29f0b65227344de4
Clayton Siby <claytonsiby@gmail.com>
2020-11-06 10:05:06 +0200

    Add validations for user & article models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,13 +62,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
