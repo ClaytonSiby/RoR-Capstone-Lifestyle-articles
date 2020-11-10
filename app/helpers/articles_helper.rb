8127fa12d8bf4cde816e9988094d1d9bc10e10a4
Clayton Siby <claytonsiby@gmail.com>
2020-11-10 08:44:19 +0200

    Add category helper methods

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,2 @@
 module ArticlesHelper
-  def remove_image?(article)
-    article.image.attached?
-  end
 end
--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -1,7 +1,15 @@
 module HomepageHelper
   def featured_article
     Article.includes(:votes).all.max do |article_one, article_two|
-      article_one <=> article_two
+      article_one.votes.count <=> article_two.votes.count
     end
+  end
+
+  def hero_image
+    featured_article ? featured_article.image : Article.no_articles_img
+  end
+
+  def featured_article_title
+    featured_article ? featured_article.title : 'No Featured Articles so far'
   end
 end
