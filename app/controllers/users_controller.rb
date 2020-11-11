447f7b19c844ed9b882304cd9475715bb7ef9ce7
Clayton Siby <claytonsiby@gmail.com>
2020-11-11 18:35:46 +0200

    Add style to homepage & category show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        format.html { redirect_to root_path, flash[:notice] = 'User was successfully created.' }
+        format.html { redirect_to root_path, notice: 'You have signed up successfully' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
