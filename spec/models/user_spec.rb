9a784eeee43922c424eafc3a9c7f24461720df58
Clayton Siby <claytonsiby@gmail.com>
2020-11-12 11:46:55 +0200

    Write & run tests for the user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:articles) }
+    it { should have_many(:votes) }
+  end
+end
