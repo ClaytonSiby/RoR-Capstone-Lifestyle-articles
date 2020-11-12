8e78b598e19810dcffd099d5fa42218ad840b85f
Clayton Siby <claytonsiby@gmail.com>
2020-11-12 11:56:48 +0200

    Add rspec tests for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true
+  validates :priority, presence: true
 
   has_many :categories_articles
   has_many :articles, through: :categories_articles, dependent: :destroy
